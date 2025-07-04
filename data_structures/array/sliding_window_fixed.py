4e4074a835982f619d9712d25395ae7fce2be313
Brett Hoes <bretthoes@gmail.com>
2025-07-04 08:59:51 -0400

    add sliding window fixed

---
+++ data_structures/array/sliding_window_fixed.py
@@ -0,0 +1,29 @@
+"""
+Sliding Window Fixed
+
+Given an array, return true if there are two elements within a window of size k that are equal.
+
+"""
+
+arr = [1, 2, 3, 2, 3, 3]
+k = 2
+# true
+
+def has_duplicate_within_window(nums, k):
+    """
+    Return True if any value appears twice inside any contiguous
+    subarray of length k, otherwise False.
+    O(n)
+    """
+    vals = set()
+    for R, num in enumerate(nums):
+        if R >= k:
+            vals.discard(nums[R - k])
+
+        if num in vals:
+            return True
+
+        vals.add(num)
+
+    return False
+
