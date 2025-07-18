8b85cefec103467950a4934eedf729634caef62a
Brett Hoes <bretthoes@gmail.com>
2025-07-17 21:52:03 -0400

    rename dijkstras

--- data_structures/graph/dijkstra.py
+++ data_structures/graph/dijkstra.py
@@ -1,5 +1,5 @@
 """
-Djikstra's Algorithm (shortest path)
+dijkstra's Algorithm (shortest path)
 BFS is not sufficient for finding the shortest path between two nodes when the edges have weights.
 i.e., if we have the graph below and we want the shortest path from 'a' to 'd':
     a -> b
@@ -14,9 +14,9 @@ The SHORTEST path is just a -> d, but what if we add weights to the edges?
     c -2-> d
 
 The "shortest" or most efficient path is now a -> c then c -> d, even though it involves more traversals.
-This is where Djikstra's comes in.
+This is where dijkstra's comes in.
 
-The idea behind Djikstra's is having a "frontier" of nodes that we've visited so far,
+The idea behind dijkstra's is having a "frontier" of nodes that we've visited so far,
 and we're tracking the shortest path to these nodes so far.
 and among that frontier, we want to consider all the outgoing edges,
 and what the shortest path is - not just for the cost of the next jump, but the total cost from the start
