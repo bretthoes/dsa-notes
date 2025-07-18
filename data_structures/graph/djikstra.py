bee17a8bcf7caa44361f0300ac7c4bce162f2ae2
Brett Hoes <bretthoes@gmail.com>
2025-07-17 20:46:30 -0400

    dji

--- data_structures/graph/djikstra.py
+++ data_structures/graph/djikstra.py
@@ -24,4 +24,60 @@ to the next node.
 This is where we'll use a minheap to help track.
 We have a minheap where we have a pair of values <cost, node>
 """
+import heapq
 
+def shortestPath(edges, n, src):
+    """
+    Give na connected graph represented by a list of edges, where:
+    - edge[0] = src
+    - edge[1] = dest
+    - edge[2] = weight,
+    find the shortest path from src to every other node in the graph.
+    O(E * logV), or O(E * logE)
+    """
+    adj = {}
+    for i in range(n):
+        adj[i] = []
+
+    # s = src, d = dst, w = weight
+    for s, d, w in edges:
+        adj[s].append((w,d))
+
+    shortest = {}
+    heap = [(0, src)]
+    while heap:
+        weight, node = heapq.heappop(heap)
+        if node in shortest:
+            continue
+        shortest[node] = weight
+
+        for neighborWeight, neighbor in adj[node]:
+            if neighbor not in shortest:
+                heapq.heappush(heap, (weight + neighborWeight, neighbor))
+
+    return shortest
+
+
+edges = [
+    (0, 1, 4),
+    (0, 2, 1),
+    (1, 2, 2),
+    (1, 3, 1),
+    (2, 3, 5),
+]
+n   = 4
+src = 0
+
+# call your function
+print(shortestPath(edges, n, src))
+"""
+    (0)
+   /   \
+  4     1
+ /       \
+(1)———2——>(2)
+  |      /
+  1     5
+  |   /
+  (3)
+"""
