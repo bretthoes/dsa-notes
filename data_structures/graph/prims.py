50c92a27a413791bddccd356229d64cddb85e7eb
Brett Hoes <bretthoes@gmail.com>
2025-07-23 17:30:40 -0400

    add prims notes

--- data_structures/graph/prims.py
+++ data_structures/graph/prims.py
@@ -21,7 +21,33 @@ We can run Prim's algorithm to find out multiple things, such as:
 1. Cost of the minimum spanning tree
 2. Return the minimum spanning tree itself, as another graph, or a list of edges
 """
- 
-
+import heapq
+def minimumSpanningTree(edges, n):
+    adj = {}
+    for i in range(1, n + 1):
+        adj[i] = []
+    for src, dst, weight in edges:
+        adj[src].append([dst,weight])
+        adj[dst].append([src,weight])
+    
+    # initialize the heap by choosing a single node
+    # (in this case 1) and pushing all its neighbors
+    heap = []
+    for neighbor, weight in adj[1]:
+        heapq.heappush(heap, [weight, 1, neighbor])
+    print(adj)
+    mst = []
+    visit = set()
+    visit.add(1)
+    while heap:
+        weight, src, node = heapq.heappop(heap)
+        if node in visit:
+            continue
+        mst.append([src, node])
+        visit.add(node)
+        for neighbor, weight in adj[node]:
+            if neighbor not in visit:
+                heapq.heappush(heap, [weight, node, neighbor])
+    return mst
 
 
